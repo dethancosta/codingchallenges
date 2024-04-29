01405cb30b8d0d36aa889d8b2e56efa1a93ced58
Ethan Costa <d.ethan.costa@gmail.com>
2024-04-28 20:20:56 -0400

    Complete Steps 1-4

--- shpp/main.cpp
+++ shpp/main.cpp
@@ -1,9 +1,27 @@
+#include <cstdlib>
+#include <experimental/filesystem>
 #include <iostream>
+#include <string.h>
 using namespace std;
+namespace fs = std::experimental::filesystem;
 
 int main() {
-    cout << "shpp> ";
     string input;
-    getline(cin, input);
-    cout << "Input: " << input << endl;
+    int result;
+    do {
+        cout << "shpp> ";
+        getline(cin, input);
+        result = system(input.c_str());
+        if (result != 0) {
+            cout << "Command did not execute successfully." << endl;
+        } else if (input.rfind("cd", 0)) { // TODO fix: needs to START WITH cd
+            size_t start = 2;
+            while(start < input.size() &&  isspace(input[start])) {
+                start++;
+            }
+            fs::current_path(input.substr(start));
+        }
+    } while (input.compare("exit"));
+
+    return result;
 }
